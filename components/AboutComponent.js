33822e2720664815504751bdceb1619ea3f5d843
tannerstuhr <tannerjstuhr@gmail.com>
2022-05-20 18:54:04 -0700

    Added loading component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,11 +3,28 @@ import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
     partners: state.partners,
   };
+};
+
+const Mission = () => {
+  return (
+    <Card title="Our Mission">
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
 };
 
 class About extends Component {
@@ -25,6 +42,27 @@ class About extends Component {
         />
       );
     };
+
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <Mission />
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -6,6 +6,7 @@ import { PROMOTIONS } from "../shared/promotions";
 import { PARTNERS } from "../shared/partners";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -15,7 +16,19 @@ const mapStateToProps = (state) => {
   };
 };
 
-function RenderItem({ item }) {
+function RenderItem(props) {
+  const { item } = props;
+
+  if (props.isLoading) {
+    return <Loading />;
+  }
+  if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    );
+  }
   if (item) {
     return (
       <Card featuredTitle={item.name} image={{ uri: baseUrl + item.image }}>
@@ -49,6 +62,8 @@ class Home extends Component {
               (campsite) => campsite.featured
             )[0]
           }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
         />
         <RenderItem
           item={
@@ -56,6 +71,8 @@ class Home extends Component {
               (promotion) => promotion.featured
             )[0]
           }
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
         />
         <RenderItem
           item={
@@ -63,6 +80,8 @@ class Home extends Component {
               (partner) => partner.featured
             )[0]
           }
+          isLoading={this.props.partners.isLoading}
+          errMess={this.props.partners.errMess}
         />
       </ScrollView>
     );
