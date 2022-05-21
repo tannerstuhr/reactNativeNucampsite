b7330d069c173a0ac2b88bbe2a65d90fa047212b
tannerstuhr <tannerjstuhr@gmail.com>
2022-05-21 09:35:46 -0700

    Redux adding favorites

--- components/LoadingComponent.js
+++ components/LoadingComponent.js
@@ -4,7 +4,7 @@ import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 function Loading() {
   return (
     <View style={styles.loadingView}>
-      <ActivityIndicator size="large" color="#5367DD" />
+      <ActivityIndicator size="large" color="#5637DD" />
       <Text style={styles.loadingText}>Loading . . .</Text>
     </View>
   );
